30917d45db5b4a7303df29cc1084a1e6fd6bd1cc
gudouhe <gudouhe@outlook.com>
2023-10-30 21:00:22 +0800

    Quartz sync: Oct 30, 2023, 9:00 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,6 +25,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    component.RecentNotes(),
   ],
   right: [
     Component.Graph(),
